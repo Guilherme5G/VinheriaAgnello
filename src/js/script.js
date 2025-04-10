46b29c521d5944c6a8f20b4624eb986c455cb95b
camila <camilamendonca777@gmial.com>
2025-04-10 18:02:42 -0300

    Atualiza HTML, JS e adiciona vinharia-agnello

--- src/js/script.js
+++ src/js/script.js
@@ -2,3 +2,21 @@ let nomeVinho = prompt("Digite o nome do vinho a ser cadastrado:")
 let tipoVinho = prompt("Qual o tipo do vinho?")
 let safraVinho = prompt("De que ano ele é?")
 let quantEstoque = prompt("Quantos vinhos há em estoque?")
+
+if (nomeVinho == "") {
+    nomeVinho = prompt('Digite o nome do vinho a ser cadastrado: ');
+}
+if (tipoVinho == "") {
+    tipoVinho = prompt('"Qual o tipo do vinho?"');
+}
+if (safraVinho == "") {
+    safraVinho = prompt("De que ano ele é?");
+}
+if (quantEstoque == "") {
+    quantEstoque = prompt("Quantos vinhos há em estoque?");
+}
+
+if (quantEstoque <= 5) {
+    alert('VISUALIZE O CONSOLE!');
+    console.log('QUANTIDADE BAIXA NO ESTOQUE!');
+}
