5f71efe73099c94b873e5feb399b3967f4f05c62
Yan Breno <ybarutti2809@gmail.com>
2025-04-10 09:40:23 -0300

    Pedindo informações do vinho

---
+++ src/js/script.js
@@ -0,0 +1,4 @@
+let nomeVinho = prompt("Digite o nome do vinho a ser cadastrado:")
+let tipoVinho = prompt("Qual o tipo do vinho?")
+let safraVinho = prompt("De que ano ele é?")
+let quantEstoque = prompt("Quantos vinhos há em estoque?")
